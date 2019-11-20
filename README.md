14f33403f4c0662ea7fdb82438e30f1a85d629ee
art <artvpublic@gmail.com>
2019-11-20 23:45:21 +0300

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# SICP Exercises (Racket)
+
+### Requirements
+
+* Racket
+* Make
+
+### Installation
+
+On Ubuntu Linux:
+`apt install racket`
+
+### Using
+
+1. To run tests `make test`
+
---
+++ exercises/chapter1/1.3.rkt
@@ -0,0 +1,25 @@
+#lang racket
+(require rackunit)
+
+#| TASK: |#
+#| Define a procedure that takes three numbers as arguments |#
+#| and returns the sum of the squares of the two larger numbers. |#
+
+#| SOLUTION: |#
+(define (abs x)
+  (cond ((< x 0) (- x))
+        (else x)))
+
+(define (sumOfSquares a b)
+  (+ (* a a) (* b b)))
+
+(define (solution a b c)
+  (cond ((and (> a c) (> b c)) (sumOfSquares a b))
+        ((and (> a b) (> c b)) (sumOfSquares a c))
+  (else (sumOfSquares b c))))
+
+
+#| TESTS: |#
+(check-equal? (solution 1 2 3) (sumOfSquares 2 3) "test1")
+(check-equal? (solution 1 2 -3) (sumOfSquares 1 2) "test2")
+(check-equal? (solution -1 2 2) (sumOfSquares 2 2) "test3")
